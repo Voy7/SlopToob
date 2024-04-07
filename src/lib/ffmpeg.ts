c2ab804879818984f2799cc4a11738cf3ccfc6a2
Voy7 <wkulak3@gmail.com>
2024-04-07 03:28:52 -0500

    ffmpeg test

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,8 +1,8 @@
 import ffmpeg from 'fluent-ffmpeg'
-import ffmpegStatic from 'ffmpeg-static'
+// import ffmpegStatic from 'ffmpeg-static'
 import Env from '@/EnvVariables'
 
-if (Env.FFMPEG_PATH) ffmpeg.setFfmpegPath(Env.FFMPEG_PATH)
-else ffmpeg.setFfmpegPath(ffmpegStatic!)
+// if (Env.FFMPEG_PATH) ffmpeg.setFfmpegPath(Env.FFMPEG_PATH)
+// else ffmpeg.setFfmpegPath(ffmpegStatic!)
 
 export default ffmpeg
