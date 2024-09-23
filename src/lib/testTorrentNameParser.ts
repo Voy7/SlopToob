889330ca6a1334361bcc2f655e82fd8fd423c35a
Voy7 <wkulak3@gmail.com>
2024-09-22 22:02:53 -0500

    Update testTorrentNameParser.ts

--- src/lib/testTorrentNameParser.ts
+++ src/lib/testTorrentNameParser.ts
@@ -2,14 +2,16 @@ import 'colors'
 import parseTorrentName from '@/lib/parseTorrentName'
 
 const EXPECT = {
-  'C;/Videos/Family Guy_s10e03_random_title.mp4': 'Family Guy - S10E03 - Random Title',
+  'C:/Videos/Family_Guy_s10e03_random_title.MP4': 'Family Guy - S10E03 - Random Title',
+  'C:/Videos/Family_Guy_10x3_random_title.mkv': 'Family Guy - S10E03 - Random Title',
+  '/mnt/yt/4F8jxx91mmk389z1-1-video.mp4': '4F8jxx91mmk389z1-1-video',
   'American Dad (2005) S20e01 Fellow Traveler (1080p Dsnp Webrip X265 10bit Eac3 5 1 - Goki)[Taoe].mkv':
     'American Dad (2005) - S20E01 - Fellow Traveler',
   'The.Eric.Andre.Show.S01E07.1080p.WEB-DL.AAC2.0.H.264-BTN': 'The Eric Andre Show - S01E07',
   'SpongeBob Squarepants S03E01 - Mermaid Man and Barnacle Boy IV':
     'SpongeBob Squarepants - S03E01 - Mermaid Man And Barnacle Boy IV',
   'S01E01 Desmonds Big Day Out (2022)': 'S01E01 - Desmonds Big Day Out (2022)',
-  'Smiling Friends S02e01 1080p Hdtv Dd5 1 H 264-Intuol': 'Smiling Friends - S02E01 -'
+  'Smiling Friends s02e01 Hdtv Dd5 1 H 264-Intuol': 'Smiling Friends - S02E01'
 }
 
 console.clear()
@@ -36,7 +38,7 @@ for (const [input, expected] of Object.entries(EXPECT)) {
 }
 
 if (failCount) {
-  console.log(`\n${failCount} test(s) failed!`.white.bgRed)
+  console.log(`\n${failCount}/${Object.keys(EXPECT).length} test(s) failed!`.white.bgRed)
   process.exit(1)
 }
 console.log(`\nAll tests passed!`.white.bgGreen)
