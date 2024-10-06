a95d2d85a2b3fa9f843fd982081cb6e353464db9
Voy7 <wkulak3@gmail.com>
2024-10-06 00:38:43 -0500

    Removed space balls theme placeholder

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -2,5 +2,4 @@ export const themes = [
   { name: 'Fox News', id: 'FoxNews' },
   { name: 'Saul Goodman', id: 'SaulGoodman' },
   { name: 'Zoomer Stim', id: 'Zoomer' }
-  // { name: 'Space Balls', id: 'SpaceBalls' },
 ] as const
